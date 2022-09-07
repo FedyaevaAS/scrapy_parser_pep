6e5aad2f6f53a7f8a2f4f61cb1b02042b50f28d7
Анастасия <fedyaevaAS@yandex.ru>
2022-09-07 14:07:08 +0300

    commit 4

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,6 +1,8 @@
+import csv
 import datetime as dt
 import os
 
+from collections import defaultdict
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parent.parent
@@ -8,26 +10,24 @@ BASE_DIR = Path(__file__).parent.parent
 
 class PepParsePipeline:
     def open_spider(self, spider):
-        self.status_dict = {}
+        self.status_dict = defaultdict(int)
 
     def process_item(self, item, spider):
         status = item['status']
-        if status in self.status_dict:
-            self.status_dict[status] += 1
-        else:
-            self.status_dict[status] = 1
+        self.status_dict[status] += 1
         return item
 
     def close_spider(self, spider):
+        time_format = dt.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
         filename = os.path.join(
             BASE_DIR,
             'results',
             ('status_summary_'
-             f'{dt.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")}.csv')
+             f'{time_format}.csv')
         )
-        with open(filename, mode='w', encoding='utf-8') as f:
-            f.write('Статус,Количество\n')
-            total = sum(self.status_dict.values())
-            for status, quantity in self.status_dict.items():
-                f.write(f'{status}, {quantity}\n')
-            f.write(f'Total,{total}\n')
+        total = sum(self.status_dict.values())
+        with open(filename, mode='w', encoding='utf-8', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(('Статус', 'Количество'))
+            writer.writerows(self.status_dict.items())
+            writer.writerow(('Total', total))
